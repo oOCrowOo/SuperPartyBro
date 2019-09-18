822405ac7db28efb99b52121868ed546b078e0f8
wangziz2 <zizhen@student.unimelb.edu.au>
2019-09-18 18:42:58 +1000

    Basic UI iteraction works

--- Assets/Scripts/Menu/MainMenuManager.cs
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+
+
+    public GameObject main_panel;
+    public GameObject about_panel;
+    public GameObject create_join_panel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,41 @@ public class MainMenuManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void onClickStart(){
+        create_join_panel.SetActive(true);
+        main_panel.SetActive(false);
+    }
+
+    public void onClickAbout(){
+        about_panel.SetActive(true);
+        main_panel.SetActive(false);
+    }
+
+    public void onClickBack_Create_Join(){
+        create_join_panel.SetActive(false);
+        main_panel.SetActive(true);
+    }
+
+    public void onClickBack_About(){
+        about_panel.SetActive(false);
+        main_panel.SetActive(true);
+    }
+
+    public void onClickQuit(){
+        Application.Quit();
+    }
+
+    public void onClickCreate(){
+        //TODO: Temp code just loading the ar scene
+        // Should create the room
+        SceneManager.LoadScene("HelloAR");
+    }
+
+    public void onClickJoin(){
+        //TODO: Temp code just loading the ar scene
+        // should search for room can be joined
+        SceneManager.LoadScene("HelloAR");
     }
 }
