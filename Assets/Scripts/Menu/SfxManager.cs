b356ca9de37b7ddd0607b7735b5cd61e36ff1faa
wangziz2 <zizhen@student.unimelb.edu.au>
2019-10-07 17:09:02 +1100

    Menu bgm and button click sfx

---
+++ Assets/Scripts/Menu/SfxManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void playSFX(AudioClip clip){
+        this.GetComponent<AudioSource>().clip = clip;
+        this.GetComponent<AudioSource>().Play();
+    }
+}
