ef0b6bd16b5a7f30f8079339cf7b42177dccdeef
wangziz2 <zizhen@student.unimelb.edu.au>
2019-10-07 16:53:24 +1100

    Coke spray with SFX

---
+++ Assets/Scripts/Game/cokeshakerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cokeshakerController : MonoBehaviour
+{
+
+
+    public AudioClip sprayClip;
+    public GameObject sprayedCokeParticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spray());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator spray(){
+        sprayedCokeParticle.SetActive(true);
+        this.GetComponent<AudioSource>().clip = sprayClip;
+        this.GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(sprayClip.length);
+        sprayedCokeParticle.SetActive(false);
+    }
+}
