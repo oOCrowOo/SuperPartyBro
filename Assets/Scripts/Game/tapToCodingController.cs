3ea6bbf1ff7757c0980825c4df9ad16a6f363d2a
wangziz2 <zizhen@student.unimelb.edu.au>
2019-10-21 00:25:59 +1100

    tap to coding scripts

---
+++ Assets/Scripts/Game/tapToCodingController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class tapToCodingController : MonoBehaviour
+{
+
+    const string PYTHONCODE = @"
+def partition(arr,low,high): 
+    i = ( low-1 )
+    pivot = arr[high]
+    for j in range(low , high): 
+        if   arr[j] <= pivot: 
+            i = i+1 
+            arr[i],arr[j] = arr[j],arr[i] 
+  
+    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    return ( i+1 ) 
+
+def quickSort(arr,low,high): 
+    if low < high: 
+        pi = partition(arr,low,high) 
+        quickSort(arr, low, pi-1) 
+        quickSort(arr, pi+1, high) 
+    ";
+
+    private int count = 0;
+    private int codeLength = 0;
+
+    public Text output;
+    // Start is called before the first frame update
+    void Start()
+    {
+        codeLength = PYTHONCODE.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count < codeLength){
+            output.text = PYTHONCODE.Substring(0,count);
+        }
+    }
+
+    public void tap(){
+        count += 2;
+        while(PYTHONCODE[count] == ' '){
+            count+=1;
+        }
+    }
+}
